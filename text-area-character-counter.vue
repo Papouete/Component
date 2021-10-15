34e8d1c0b87223632eaafcd579b769b45c8c24aa
Papouete <flo.mor97@gmail.com>
2021-10-15 14:44:44 +0200

    component

---
+++ text-area-character-counter.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+    <v-textarea 
+        v-model="text"
+        :maxLength="maxLength"
+        outlined 
+        placeholder="Mon message..."
+        no-resize
+        hide-details="">
+    </v-textarea>
+    <div class="counter pt-2" v-show="hasCharactersRemaining">{{totalCharacters}}/{{maxLength}} caractères.</div>
+    <div :class="{'max-counter': !hasCharactersRemaining}" v-show="!hasCharactersRemaining">Limite de caractères atteinte.</div>
+</div>
+</template>
+
+<script>
+
+export default {
+
+    data() {
+        return {
+            text: "",
+            maxLength: 150,
+        };   
+    },
+
+    computed: {
+        totalCharacters() {
+            return this.text.length;
+        },
+    
+        hasCharactersRemaining() {
+            return this.totalCharacters < this.maxLength
+        },
+
+    },
+
+    watch: {
+        text() {
+            this.text = this.text.substring(0,this.maxLength)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+.max-counter {
+    color: red;
+}
+
+.counter {
+    color:#6b5cd7;
+    font-weight: bold;
+}
+
+</style>
---
+++ text-field-color-picker.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <v-text-field placeholder="Votre couleur de fond." rounded outlined v-model="isFontColorSelected" hide-details="" class="ma-0 pl-2">                                             
+                    <template v-slot:append>
+                        <v-menu  v-model="menu" top nudge-bottom="105" nudge-left="16" :close-on-content-click="false">
+                            <template v-slot:activator="\{ on \}">
+                                <div :style="switchColor" v-on="on" />
+                            </template>
+                            <v-card>
+                                <v-card-text class="pa-0">
+                                    <v-color-picker v-model="isFontColorSelected" flat />
+                                </v-card-text>
+                            </v-card>
+                        </v-menu>
+                    </template>
+        </v-text-field>
+</div>
+</template>
+
+<script>
+
+export default {
+    data: () => {
+        return {
+            menu:false,
+            isFontColorSelected: '#FBCEB6',
+        };
+    },
+    computed: {
+        switchColor() {
+            return {
+                backgroundColor: this.isFontColorSelected,
+                cursor: 'pointer',
+                height: '30px',
+                width: '30px',
+                borderRadius: this.menu ? '50%' : '4px',
+                transition: 'border-radius 200ms ease-in-out'
+
+                // Pour Bind la couleur choisie à un élément, utiliser :style="{backgroundColor: isFontColorSelected}"
+
+            };
+        },
+    },
+},
+
+</script>
